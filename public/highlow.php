9b6bf26b403635094026d679cc8e49cc5cc2671c
Jessica Sattler <jessicasattler28@gmail.com>
2016-08-30 15:21:49 -0500

    If user passes two arguments that are numbers in the command line, then the high low game will take those two arguments and set the min and max variables. Else if the user doesn't pass arguments, then the min and max will default to 1 and 100.

--- public/highlow.php
+++ public/highlow.php
@@ -12,12 +12,27 @@
 
 //generates random number between 1 and 100
 //we set it equal to a variable to be able to remember what that value is
-$randomNumber = mt_rand (1, 100);
+
+if ($argc == 3){
+
+$min = $argv[1];
+$max = $argv[2];
+
+var_dump($argc);
+var_dump($argv);
+}else{
+	$min = 1;
+	$max = 100;
+}
+
+
+
+$randomNumber = mt_rand ($min, $max);
 // echo ($randomNumber).PHP_EOL;
 
-fwrite(STDOUT, 'Guess a number between 1 and 100 ');
-$userGuess = fgets(STDIN);
-fwrite (STDOUT, "You guessed $userGuess").PHP_EOL;
+fwrite(STDOUT, "Guess a number between $min and $max ");
+$userGuess = trim(fgets(STDIN));
+fwrite (STDOUT, "You guessed $userGuess".PHP_EOL);
 
  
 while($userGuess !== $randomNumber){
@@ -28,19 +43,33 @@ if ($userGuess < $randomNumber){
 	echo "HIGHER".PHP_EOL;
 
 	fwrite(STDOUT, 'Guess again please ');
-	$userGuess = fgets(STDIN);
+	$userGuess = trim(fgets(STDIN));
 
 }else if($userGuess > $randomNumber){
 	echo "LOWER".PHP_EOL;
 
 	fwrite(STDOUT, 'Guess again please ');
-	$userGuess = fgets(STDIN);
+	$userGuess = trim(fgets(STDIN));
 
 }else{
 	echo "GOOD GUESS! YOU WON!";
-	exit(0); 
+	exit (0);
+
+	// fwrite(STDOUT, "Would you like to continue playing? yes or no? ");
+	// $userPlay = trim(fgets(STDIN));
+	// if($userPlay = no){
+	// exit(0); 
+		
+	// }else if($userPlay = yes){
+
+	// }
+	
 }
 
 
 };
+
+//ask user if they want to play again
+//keep track of their guesses 
+//make it pretty 
  ?>
