b44ebd7c0dfb3b9e1be3694563cbfdbe705d120c
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-11 14:55:44 -0600

    Created landing page and landing css, created a couple of divs

---
+++ public/memory.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Memory!</title>
+	<link rel="stylesheet" type="text/css" href="/css/landing.css">
+</head>
+<body>
+	<div class="container">
+		<div class="col-xs-7">
+			<h4>Regular Show</h4>
+		</div>	
+
+		<div class="col-xs-5 col-xs-offset-7">
+			<h4>Regular Show</h4>
+		</div>	
+
+		<div class="col-xs-5 col-xs-offset-7">
+			<h4>Regular Show</h4>
+		</div>	
+	
+		<div class="col-xs-5 col-xs-offset-7">
+			<h4>Regular Show</h4>
+		</div>	
+	</div>
+
+</body>
+</html>
