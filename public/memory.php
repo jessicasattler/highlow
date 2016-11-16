273733cc1de667afe819331779d988979bb4e49c
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-16 12:02:55 -0700

    Added lg column sizes so that 6 cards show up on each row when the screen is large. Also added js files

--- public/memory.php
+++ public/memory.php
@@ -12,131 +12,135 @@
 	<div class="container">
 <!-- 	row -->
 		<div class="row"> 
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <div class="col-xs-offset-2 col-xs-7"> -->
 					<!-- </div> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 1</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 1</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 1</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card">
 					<!-- <h6>Regular Show 2</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 2</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 2</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 2</h6> -->
 				</div>	
 			</div>
 
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card">
 					<!-- <h6>Regular Show 3</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6  col-sm-3">
+			<div class="col-xs-6  col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 3</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 3</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 2</h6> -->
 				</div>	
 			</div>
 
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card">
 					<!-- <h6>Regular Show 6</h6>	 -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 6</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 6</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 2</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card">
 					<!-- <h6>Regular Show 6</h6>	 -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 6</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 6</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 2</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card">
 					<!-- <h6>Regular Show 6</h6>	 -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 6</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h6>Regular Show 6</h6> -->
 				</div>	
 			</div>
-			<div class="col-xs-6 col-sm-3">
+			<div class="col-xs-6 col-sm-3 col-lg-2">
 				<div class="card ">
 					<!-- <h4>Regular Show 2</h4> -->
 				</div>	
 			</div>
 		</div> <!-- closes row -->
 	</div> <!-- closes container -->
+
+	<script src="/js/jQuery.js"></script>
+	<script src="/js/boostrap.min.js"></script>
+	<script src="/js/memory.js"></script>
 
 </body>
 </html>
