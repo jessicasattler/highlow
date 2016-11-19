f196d1509a5956bdd7f5073ead830bdff5524a74
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-19 01:13:54 -0600

    Start button fades out when it is pressed

--- public/js/memory.js
+++ public/js/memory.js
@@ -2,8 +2,9 @@
 $(document).ready(function() {
 
 	$('.start').click(function(){
-	// cards only become clickable after "start" button has been pressed
-		alert('start button was clicked');
+		//start button fades out after it is pressed
+		$('.start').fadeOut();
+		// cards only become clickable after "start" button has been pressed
 		$('.card').click(function(){
 		// when a card is clicked, background-image of card changes
 		$(this).css('background-image', 'url("/img/rigby.png")');
