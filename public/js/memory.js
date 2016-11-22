07d49c402c9c96ba3af6bdfb5786bb9f2a730b91
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-21 20:31:43 -0600

    Added comments to the js

--- public/js/memory.js
+++ public/js/memory.js
@@ -17,7 +17,8 @@ $(document).ready(function() {
 			shuffled[j] = "";
 		}
 
-
+		//Iterate through the loop the same amount of times as the amount of characters we have in pictures array and assign elements to the shuffled array 
+		//at random by choosing random indeces from the pictures array and assigning them in that order
 		for(i=0;i<listOfPictures.length;i+=1){
 			var randomNumber = parseInt((Math.random()*listOfPictures.length)+0);
 
@@ -33,6 +34,7 @@ $(document).ready(function() {
 
 	var shuffled = shufflePictures(pictures);
 
+	//assigns characters from the shuffled array to div elements at random
 	shuffled.forEach(function(element,index, array){
 		if(index % 2 == 0){
 			$("#firstContainer").prepend("<div class=\"col-xs-10 col-sm-6 col-lg-3\"><div class=\"card \" data-character=\""+element+"\"></div>	</div>");
