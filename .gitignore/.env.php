547e419c9ff679e2be9ab855fecd1855ed6fcad0
Jessica Sattler <jessicasattler28@gmail.com>
2016-12-01 09:52:16 -0600

    Sensitive info is stored in the proper places

---
+++ .gitignore/.env.php
@@ -0,0 +1,12 @@
+<?php 
+
+return array(
+
+	'DB_HOST' => 'localhost',
+	'DB_NAME' => 'Memory',
+	'DB_USER' => 'vagrant',
+	'DB_PASS' => 'vagrant',
+
+	'USER_PASS' => 'vagrant',
+
+);
---
+++ public/API.php
@@ -0,0 +1,17 @@
+<?php 
+//php script to recieve http requests and fetch data from the database
+require_once __DIR__ . '/../.env.php';
+
+$dbc = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$name = $_GET['name'];
+$retrievePics = $dbc->query("SELECT * FROM characters WHERE character_name = '$name' ");
+$retrieved = $retrievePics->fetch();
+
+var_dump($retrieved);
+
+ ?>
+
