f6819a3b921a5decf945597445b2d7a06dc5df35
Jessica Sattler <jessicasattler28@gmail.com>
2017-07-30 16:40:58 -0500

    Changing the position of the start button

--- public/memory.php
+++ public/memory.php
@@ -12,11 +12,12 @@
 	<div class="container firstContainer secondContainer">
 <!-- 	row -->
 		<div class="row"> 
-			<div class="container col-xs-6" id="firstContainer">
-
+			<!-- <div class="container col-xs-1 col-md-2 col-lg-2" id="firstContainer"> -->
+			<div class="container col-xs-2" id="firstContainer">
 
 			</div> <!--  closes first smaller container -->
-			<div class="container col-xs-6" id="secondContainer">
+			<!-- <div class="container col-xs-1 col-md-2 col-lg-2" id="secondContainer"> -->
+			<div class="container col-xs-2" id="firstContainer">
 			<button type="button" class="btn btn-lg btn-danger 	 start">START</button>
 			<h2 id="winningMessage">Congratulations! You won!</h2>
 			</div>  <!-- closes second smaller container -->
